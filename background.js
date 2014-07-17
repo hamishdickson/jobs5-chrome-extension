e04e4f96add782b7079ff9c94d70c20e731d17eb
Hamish Dickson <hamish.dickson@gmail.com>
2014-07-17 17:19:39 +0100

    first commit

---
+++ background.js
@@ -0,0 +1,25 @@
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: 'Jobs5: open job %s'
+  });
+}
+
+resetDefaultSuggestion();
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  // Suggestion code will end up here.
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+  resetDefaultSuggestion();
+});
+
+function navigate(url) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  navigate("http://localhost:3000/pages/bigDetail/" + text);
+});
